27f00dbde3285f7d102442e12e1353a2b8b923b4
tetsuyayamamoto0324 <manpuku769@gmail.com>
2025-06-08 13:52:02 +0900

    途中2

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { resolve } from "path";
 
 export interface Task {
   id: number;
@@ -10,7 +11,11 @@ const tasks: Task[] = [
   { id: 2, name: "ランニング" },
 ];
 
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 export const GET = async () => {
+  await sleep(3000);
   return NextResponse.json(
     { tasks },
     {
---
+++ src/app/sa/page.tsx
@@ -0,0 +1,12 @@
+const ServerActionsPage = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" id='name' name="name" className="bg-gray-200" />
+            <button type="submit" className="bg-gray-400 ml-2 px-2">送信</button>
+        </form>
+    </div>
+  )
+}
+
+export default ServerActionsPage
