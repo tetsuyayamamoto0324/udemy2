664e8fdcbe4f42c6c05b6f745a7ac7bbe8152f34
kanno shoki <shou.k.0322@gmail.com>
2025-06-08 15:21:27 +0900

    途中3

---
+++ src/actions/sampleActions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+interface FormState {
+  error: string;
+}
+export const createTask = async (
+  taskId: number,
+  state: FormState,
+  formData: FormData
+) => {
+  console.log("タスクを作成しました");
+  console.log(formData.get("name"));
+  console.log(taskId);
+  state.error = "エラーが発生しました";
+  return state;
+};
